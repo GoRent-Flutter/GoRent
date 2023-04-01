744f1a74027c8288e2c8e944b8f6e3d3440d8334
may <may.awadallah@outlook.com>
2023-04-01 04:27:12 +0300

    mayVer

--- lib/screens/Main/main_screen.dart
+++ lib/screens/Main/main_screen.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:gorent_application1/constraints.dart';
+import 'package:gorent_application1/screens/RentList/rentlist_screen.dart';
+
+import '../BuyList/buylist_screen.dart';
 
 class MainScreen extends StatelessWidget {
   const MainScreen({Key? key}) : super(key: key);
-
 
   @override
   Widget build(BuildContext context) {
@@ -38,97 +40,128 @@ class MainScreen extends StatelessWidget {
                   ),
                 ],
               ),
-              
-            child: Stack(
-      children: [
-        Positioned(
-          top: 5,
-          left: 5,
-          right: 5,
-          bottom: 30,
-          child: Container(
-            decoration: BoxDecoration(
-              color: primaryRed,
-              borderRadius: BorderRadius.circular(24.0),
-            ),
-          ),
-        ),
-          Center(
-          // heightFactor: 1.0,
-                        child: Column(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: const [
-                    Padding(
-                      padding: EdgeInsets.only(bottom: 2),
-                      child: Text(
-                        "شراء عقار",
-                        style: TextStyle(
-                          fontSize: 17,
-                          color: primaryRed,
-                          decoration: TextDecoration.none,
-                        ),
+              child: Stack(
+                children: [
+                  Positioned(
+                    top: 5,
+                    left: 5,
+                    right: 5,
+                    bottom: 30,
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: primaryRed,
+                        borderRadius: BorderRadius.circular(24.0),
                       ),
                     ),
-                  ],
-                ),
+                  ),
+                  Positioned(
+                    // top: -40,
+                    // left: -50,
+                    child: GestureDetector(
+                      child: Center(
+                        // heightFactor: 1.0,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: const [
+                            Padding(
+                              padding: EdgeInsets.only(bottom: 2),
+                              child: Text(
+                                "شراء عقار",
+                                style: TextStyle(
+                                  fontSize: 17,
+                                  color: primaryRed,
+                                  decoration: TextDecoration.none,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      onTap: () {
+                        Navigator.of(context).pushReplacement(MaterialPageRoute(
+                          builder: (BuildContext context) => BuyListScreen(),
+                        ));
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ),
-      ],
-                ),
-          ),),
           Positioned(
             top: 130,
             left: 207,
             right: 25,
+            // child: InkWell(
+            //   onTap: () {
+            //     Navigator.push(
+            //       context,
+            //       MaterialPageRoute(
+            //           builder: (context) => const WelcomeScreen()),
+            //     );
+            //   },
             child: Container(
-                height: 177,
-                decoration: BoxDecoration(
-                  color: primaryWhite,
-                  borderRadius: BorderRadius.circular(24.0),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.black.withOpacity(0.3),
-                      spreadRadius: 2,
-                      blurRadius: 5,
-                    ),
-                  ],
-                ),
-              
- child: Stack(
-      children: [
-        Positioned(
-          top: 5,
-          left: 5,
-          right: 5,
-          bottom: 30,
-          child: Container(
-            decoration: BoxDecoration(
-              color: primaryRed,
-              borderRadius: BorderRadius.circular(24.0),
-            ),
-          ),
-        ),
-          Center(
-          // heightFactor: 1.0,
-                        child: Column(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: const [
-                    Padding(
-                      padding: EdgeInsets.only(bottom: 2),
-                      child: Text(
-                        "إستئجار عقار",
-                        style: TextStyle(
-                          fontSize: 17,
-                          color: primaryRed,
-                          decoration: TextDecoration.none,
-                        ),
+              height: 177,
+              decoration: BoxDecoration(
+                color: primaryWhite,
+                borderRadius: BorderRadius.circular(24.0),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(0.3),
+                    spreadRadius: 2,
+                    blurRadius: 5,
+                  ),
+                ],
+              ),
+              child: Stack(
+                children: [
+                  Positioned(
+                    top: 5,
+                    left: 5,
+                    right: 5,
+                    bottom: 30,
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: primaryRed,
+                        borderRadius: BorderRadius.circular(24.0),
                       ),
                     ),
-                  ],
-                ),
+                  ),
+                  Positioned(
+                    // top: -40,
+                    // left: -50,
+                    child: GestureDetector(
+                      child: Center(
+                        // heightFactor: 1.0,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: const [
+                            Padding(
+                              padding: EdgeInsets.only(bottom: 2),
+                              child: Text(
+                                "إستئجار عقار",
+                                style: TextStyle(
+                                  fontSize: 17,
+                                  color: primaryRed,
+                                  decoration: TextDecoration.none,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      onTap: () {
+                        Navigator.of(context).pushReplacement(MaterialPageRoute(
+                          builder: (BuildContext context) => RentListScreen(),
+                        ));
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ),
-      ],
-                ),
-          ),),
+          // ),
         ])));
   }
 }
